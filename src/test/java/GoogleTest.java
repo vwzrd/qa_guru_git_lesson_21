2e073edc9de90fa81b9930a2b174ce6fc9d4b1bb
Maxim_Gavrikov <destrallhumn@gmail.com>
2023-07-25 18:52:36 +0300

    added Google Test

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class GoogleTest {
+
+    static {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @Test
+    void googleTest() {
+        Selenide.open("https://google.com");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+
+    }
+}
