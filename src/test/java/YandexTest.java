5eef064109b7f77f94d6eadc4687dc6384ec3171
Maxim_Gavrikov <destrallhumn@gmail.com>
2023-07-25 19:25:41 +0300

    added empty Yandex Test

---
+++ src/test/java/YandexTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+
+public class YandexTest {
+
+    static {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @Test
+    void yaTest() {
+
+    }
+}
